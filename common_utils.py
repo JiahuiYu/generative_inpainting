838dbb70a5d0a83030e98a4eac0a96dbff965e70
arikolsh <arolshan@microsoft.com>
2018-07-29 21:45:32 +0300

    no gpu

--- common_utils.py
+++ common_utils.py
@@ -1,6 +1,6 @@
 import errno
 import os
-
+import cv2
 
 def open_path_or_create(path_dir):
     if not os.path.exists(os.path.dirname(path_dir)):
@@ -9,3 +9,9 @@ def open_path_or_create(path_dir):
         except OSError as exc:  # Guard against race condition
             if exc.errno != errno.EEXIST:
                 raise
+
+# def get_dims(src):
+#     a = cv2.imread(src, cv2.IMREAD_ANYCOLOR)
+#     print(a)
+#
+# get_dims("training_data/training/textures/1.png")
