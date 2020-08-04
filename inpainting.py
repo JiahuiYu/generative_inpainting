bc979fb44e9c347727df69bb3f921ad0584fb8de
woomurf <dngusdnd@gmail.com>
2020-08-04 17:02:46 +0900

    preload session

--- inpainting.py
+++ inpainting.py
@@ -4,17 +4,12 @@ import cv2
 import numpy as np
 import tensorflow as tf
 import neuralgym as ng
+import time
 
 from inpaint_model import InpaintCAModel
 
-def inpaint(image, mask, model, assign_ops, sess_config):
+def inpaint(image, mask, model, sess, g, checkpoint):
     FLAGS = ng.Config('inpaint.yml')
-
-
-    # model = InpaintCAModel()
-    # image = cv2.imread(args.image)
-    # mask = cv2.imread(args.mask)
-
 
     assert image.shape == mask.shape
 
@@ -28,26 +23,23 @@ def inpaint(image, mask, model, assign_ops, sess_config):
     mask = np.expand_dims(mask, 0)
     input_image = np.concatenate([image, mask], axis=2)
 
-    # sess_config = tf.ConfigProto()
-    # sess_config.gpu_options.allow_growth = True
-    with tf.Session(config=sess_config) as sess:
-        print("tf sesstion start")
+    with g.as_default():
         input_image = tf.constant(input_image, dtype=tf.float32)
-        output = model.build_server_graph(FLAGS, input_image)
+        output = model.build_server_graph(FLAGS, input_image, reuse=tf.AUTO_REUSE)
         output = (output + 1.) * 127.5
         output = tf.reverse(output, [-1])
         output = tf.saturate_cast(output, tf.uint8)
-        # load pretrained model
-        # vars_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
-        # assign_ops = []
-        # for var in vars_list:
-        #     vname = var.name
-        #     from_name = vname
-        #     var_value = tf.contrib.framework.load_variable(args.checkpoint_dir, from_name)
-        #     assign_ops.append(tf.assign(var, var_value))
+
+        tic = time.time()
+        vars_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
+        assign_ops = []
+        for var in vars_list:
+            vname = var.name
+            from_name = vname
+            var_value = tf.contrib.framework.load_variable(checkpoint, from_name)
+            assign_ops.append(tf.assign(var, var_value))
+        print("Tic toc : ", time.time() - tic)
         sess.run(assign_ops)
-        print('before run ')
         result = sess.run(output)
-        print('DONE')
         return result[0][:, :, ::-1]
-        # cv2.imwrite(args.output, result[0][:, :, ::-1])
+
--- server.py
+++ server.py
@@ -6,29 +6,23 @@ import tensorflow as tf
 from inpainting import inpaint
 import cv2
 from io import BytesIO
-
+import time
+import os 
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
 
 app = Flask(__name__, template_folder='./templates/')
 
-# pre-load model ------------------------------------
-def loadModel(checkpoint_dir):
-    sess_config = tf.ConfigProto()
-    sess_config.gpu_options.allow_growth = True
-    with tf.Session(config=sess_config) as sess:
-        model = InpaintCAModel()
-        vars_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
-        assign_ops = []
-        for var in vars_list:
-            vname = var.name
-            from_name = vname
-            var_value = tf.contrib.framework.load_variable(checkpoint_dir, from_name)
-            assign_ops.append(tf.assign(var, var_value))
-    
-    return model, assign_ops, sess_config
+# pre-load session ------------------------------------
+CHECKPOINT_DIR_PLACES2 = "./model_logs/release_places2_256_deepfill_v2"
+CHECKPOINT_DIR_CELEBA = "./model_logs/release_celeba_hq_256_deepfill_v2"
 
-places2, places2_ops, sess_config = loadModel("./model_logs/release_places2_256_deepfill_v2")
-celeba, celeba_ops, sess_config = loadModel("./model_logs/release_celeba_hq_256_deepfill_v2")
+model = InpaintCAModel()
+
+sess_config = tf.ConfigProto()
+sess_config.gpu_options.allow_growth = True
+sess = tf.Session(config=sess_config)
+g = tf.get_default_graph()
 #----------------------------------------------------
 
 
@@ -38,19 +32,19 @@ def index():
 
 @app.route("/inpainting", methods=["POST"])
 def inpainting():
-    global places2
-    global celeba
 
-    image = cv2.imdecode(np.fromstring(request.files['image'].read(), np.uint8), cv2.IMREAD_UNCHANGED)
-    mask = cv2.imdecode(np.fromstring(request.files['mask'].read(), np.uint8), cv2.IMREAD_UNCHANGED)
+    image = cv2.imdecode(np.fromstring(request.files['image'].read(), np.uint8), cv2.IMREAD_COLOR)
+    mask = cv2.imdecode(np.fromstring(request.files['mask'].read(), np.uint8), cv2.IMREAD_COLOR)
     model_name = request.form['model']
 
-    model = places2 if model_name == "places2" else celeba
-    ops = places2_ops if model_name == "places2" else celeba_ops
+    checkpoint = CHECKPOINT_DIR_PLACES2 if model_name == "places2" else CHECKPOINT_DIR_CELEBA
 
-    print("go inpaint")
-    output = inpaint(image, mask, model, ops, sess_config)
+    output = inpaint(image, mask, model, sess, g, checkpoint)
     img = cv2.cvtColor(output, cv2.COLOR_BGR2RGB)
+    
+    collections = g.get_all_collection_keys()
+    for name in collections:
+        g.clear_collection(name)
 
     result = Image.fromarray(img)
     io = BytesIO()
@@ -61,4 +55,3 @@ def inpainting():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80 )
-
