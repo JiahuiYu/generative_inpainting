577d311a11f51768bcdc41046014369d08f710ac
woomurf <dngusdnd@gmail.com>
2020-08-05 16:50:37 +0900

    painter & undo

--- static/canvas.js
+++ static/canvas.js
@@ -6,21 +6,42 @@ var pos = {
 
 var canvas, ctx; 
 
-function drawing(){
+var pointList = []
+var points = []
+
+function drawing(img){
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext('2d');
-
-    var img1 = new Image;
-    img1.src = URL.createObjectURL(document.getElementById("image").files[0]);
-    img1.onload = function() {
-        URL.revokeObjectURL(img1.src) // free memory
-        ctx.drawImage(img1,0,0)
-    }
+    ctx.clearRect(0,0, canvas.width, canvas.height);
     
+    canvas.width = img.width;
+    canvas.height = img.height;
+    
+    ctx.drawImage(img, 0, 0, img.width, img.height);
+
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = 20;
+
     canvas.addEventListener("mousedown", listener);
     canvas.addEventListener("mouseup",listener);
     canvas.addEventListener("mousemove", listener);
     canvas.addEventListener("mouseup", listener);
+
+    img.style.display = "none";
+    console.log("drawing");
+    
+}
+
+function setLineWidthSize(size) {
+    ctx.lineWidth = size;
+    console.log(ctx.lineWidth);
+    console.log(size);
+    
+    
+}
+
+function clear() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
 }
 
 function listener(event){
@@ -46,21 +67,42 @@ function initDraw(event) {
     pos.X = coors.X;
     pos.Y = coors.Y;
     ctx.moveTo(pos.X, pos.Y);
+
+    points.push({
+        x: pos.X,
+        y: pos.Y,
+        mode:"begin"
+    })
 }
 
 function draw(event) {
-    var cooors = getPostion(event);
+    var coors = getPosition(event);
     ctx.lineTo(coors.X, coors.Y);
     pos.X = coors.X;
     pos.Y = coors.Y;
-    ctx.lineWidth = 5;
     ctx.stroke();
+
+    points.push({
+        x: pos.X,
+        y: pos.Y,
+        mode: "draw"
+    })
 }
 
 function finishDraw() {
     pos.drawable = false;
-    pos.X = -1;
-    pos.Y = -1;
+    var coors = getPosition(event);
+    pos.X = coors.X;
+    pos.Y = coors.Y;
+
+    points.push({
+        x: pos.X,
+        y: pos.Y,
+        mode: "end"
+    })
+
+    pointList.push(points);
+    points = [];
 }
 
 function getPosition(event) {
@@ -70,4 +112,88 @@ function getPosition(event) {
         X:x,
         Y:y,
     }
+}
+function redrawAll(){
+
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+
+    var img = document.getElementById('hiddenImage');
+    img.style.display = "block";
+
+    ctx.drawImage(img, 0, 0, img.width, img.height);
+    img.style.display = "none";
+
+    for(var i=0;i<pointList.length;i++){
+
+        var points=pointList[i];
+        
+        for(var j=0; j<points.length; j++){
+            var pt = points[j];
+            if(pt.mode=="begin"){
+                ctx.beginPath();
+                ctx.moveTo(pt.x,pt.y);
+            }
+            ctx.lineTo(pt.x,pt.y);
+            if(pt.mode=="end" || (i==points.length-1)){
+                ctx.stroke();
+            }
+        }
+        
+    }
+    
+}
+
+function undo() {
+    pointList.pop();
+    redrawAll();
+}
+
+function createMask() {
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+
+    for(var i=0;i<pointList.length;i++){
+
+        var points=pointList[i];
+        
+        for(var j=0; j<points.length; j++){
+            var pt = points[j];
+            if(pt.mode=="begin"){
+                ctx.beginPath();
+                ctx.moveTo(pt.x,pt.y);
+            }
+            ctx.lineTo(pt.x,pt.y);
+            if(pt.mode=="end" || (i==points.length-1)){
+                ctx.stroke();
+            }
+        }
+        
+    }
+
+    return saveCanvas();
+}
+
+function saveCanvas() {
+    var img = canvas.toDataURL();
+    return img;
+}
+
+function showCanvas() {
+    canvas.style.display = 'block';
+    
+    var undo = document.getElementById("undo");
+    undo.style.display = 'inline-block';
+
+    var pointerSize = document.getElementById("pointerSize");
+    pointerSize.style.display = 'inline-block';
+}
+
+function hideCanvas() {
+    canvas.style.display = "none";
+
+    var undo = document.getElementById("undo");
+    undo.style.display = 'none';   
+    var pointerSize = document.getElementById("pointerSize");
+    pointerSize.style.display = 'none';
+
+     
 }
